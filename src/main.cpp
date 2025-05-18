fd83af04895777cdeb6dcd0c6caee11947f295e8
nitron1108 <mja29122@gmail.com>
2025-05-18 22:17:51 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
-// Copyright 2022 NNTU-CS
 #include <iostream>
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+  Train t;
 
-  while (count--)
-    train.addCar(false);
+  const int total = 60; // можно менять
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
+  for (int i = 0; i < total; ++i) {
+    t.addWagon(false);  // можно поменять на true или рандом для эксперимента
+  }
+
+  int length = t.calculateLength();
+  int ops = t.getStepCount();
+
+  std::cout << "Length of the train: " << length << '\n';
+  std::cout << "Number of steps taken: " << ops << '\n';
+
   return 0;
 }
