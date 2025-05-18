f946b22e64379c32a6389c746ba3f329f6079372
nitron1108 <mja29122@gmail.com>
2025-05-18 22:14:39 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,20 +1,23 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
 
 class Train {
  private:
-  struct Car {
-    bool light; // состояние лампочки
-    Car *next;
-    Car *prev;
+  struct Wagon {
+    bool lamp;
+    Wagon* next;
+    Wagon* prev;
   };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Car *first; // точка входа в поезд (первый вагон)
+
+  Wagon* start;
+  int stepCounter;
+
  public:
   Train();
-  void addCar(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
+
+  void addWagon(bool lampState);
+  int calculateLength();
+  int getStepCount() const;
 };
+
 #endif  // INCLUDE_TRAIN_H_
